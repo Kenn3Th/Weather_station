a92dd5c8c54978102ed9e91a95005e2ac3f55bc4
Kenneth R. Eikrehagen <45165531+Kenn3Th@users.noreply.github.com>
2019-04-26 16:11:54 +0200

    Update broker_weather.py

--- broker_weather.py
+++ broker_weather.py
@@ -4,13 +4,13 @@ import paho.mqtt.client as mqtt
 import matplotlib.pyplot as plt
 import numpy as np
  
-#Funksjon som plotter grafer av innsamlet data
+
 def plot_graf(data,fig_nr,enhet,verdier):
+    print("plotter")
     x_verdier = np.linspace(0, len(verdier)/2, len(verdier))
     plt.figure(fig_nr)
     plt.ylabel(enhet)
-    plt.xlabel("tid")
-    #plt.ylim(0,50)
+    plt.xlabel("tid [s]")
     plt.title(data)
     plt.plot(x_verdier, verdier)
     plt.savefig(data+".png")
@@ -27,37 +27,41 @@ def on_connect(client, userdata, flags, rc):
 Temp = [] 
 Humi = []
 Vind = []
+#timer = 0
 # "callback" for naar en PUBLISH beskjed blir mottatt fra en "publisher" 
 def on_message(client, userdata, msg): 
-   if msg.topic == '/ute/pi':
-       data = float(msg.payload) #motatt data
-       if data>1000:
+    global timer
+    if msg.topic == '/ute/pi':
+        data = float(msg.payload) #motatt data
+        if data>1000:
            Data = data-1000
            Vind.append(Data)
            print("Vind: "+str(Data))
-       elif 1000>data>400:
+        elif 1000>data>400:
            Data = data-500
            Temp.append(Data)
            print("Temp: "+str(Data))
-       else:
+        else:
            Humi.append(data)
            print("Fukt: "+str(data))
-   if len(Vind)>5:
+    if len(Vind)>5:
         print("Like a glove!")
-        plot_graf("Temperatur",1,"C",Temp)      #Kaller paa plott funksjonen
-        plot_graf("Fuktighet",2,"%",Humi)       #Kaller paa plott funksjonen
-        plot_graf("Vindhastighet",3,"m/s",Vind) #Kaller paa plott funksjonen
-        #konverterer innsamlet data om til en CSV som sendes til en klient
+        plot_graf("Temperatur_test",1,"C",Temp)
+        plot_graf("Fuktighet_test",2,"%",Humi)
+        plot_graf("Vindhastighet_test",3,"m/s",Vind)
         send_data = str("%0.2f" % Temp[-1])+'/'+str("%0.2f" % Humi[-1])+'/'+str("%0.2f" % Vind[-1])
+        print(send_data)
         client.publish('/info/esp8266',send_data)
-
+"""
+    timer += 1
+    print(timer)
+    time.sleep(1)
+"""
 # Lager MQTT klienter og kobler de til den "localhost"
 # "localhost": Raspberry PI som kjorer denne koden
 client = mqtt.Client()
 client.on_connect = on_connect
-if int(time.time())%10 and len(Vind)>5:
-    print("I am legend!")
-    client.on_message = on_message 
+client.on_message = on_message 
 client.connect('localhost', 1883, 60)   
 print('Script is running, press Ctrl-C to quit...')
 # Setter opp en klient-loop som gaar helt tl den manuelt blir frakoblet
